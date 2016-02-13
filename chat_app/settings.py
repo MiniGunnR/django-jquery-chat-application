82b7bfe35f990fea60a838ed71efe922b02656ba
Yusheng Ding <davidthinkleding@gmail.com>
2016-02-13 13:18:49 -0600

    chat room beta1

--- chat_app/settings.py
+++ chat_app/settings.py
@@ -77,9 +77,11 @@ WSGI_APPLICATION = 'chat_app.wsgi.application'
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+   'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'mySite',
+        'USER': 'root',
+        'PASSWORD': 'password',
     }
 }
 
